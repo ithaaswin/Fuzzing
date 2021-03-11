dd51971fbffa89a583ee7eb671a7b9a7747c0512
Chris Parnin <chris.parnin@gmail.com>
2021-03-11 17:31:22 -0500

    improve and document driver

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const mtTest = require('./lib/driver').mutationTesting;
+const fuzz = require('./lib/driver').fuzz;
 const fs = require('fs');
 
 // Code under test...
 const marqdown = require('./test/marqdown');
 
-// Inputs
+// Seed inputs
 let mdA = fs.readFileSync('test/test.md','utf-8');
 let mdB = fs.readFileSync('test/simple.md','utf-8');
 
-// Fuzz function 1000 times, given seed inputs.
-mtTest(1000, () => [mdA, mdB], (md) => marqdown.render(md) );
+// Fuzz function 1000 times, with given seed string inputs.
+fuzz(1000, [mdA, mdB], (md) => marqdown.render(md) );
